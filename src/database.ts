db6233d8e5f71a71c88575ff6fe3758dd198f5a1
SurajSulten <suraj.sulten@gmail.com>
2024-10-15 10:36:53 +0500

    fix: env file

--- src/database.ts
+++ src/database.ts
@@ -5,9 +5,9 @@ const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || 'user',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'mydb',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
   entities: [],
   synchronize: true,
 });
