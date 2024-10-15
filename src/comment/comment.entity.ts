e6946553ca638d735630acd49bb16595ab2758d1
SurajSulten <suraj.sulten@gmail.com>
2024-10-15 10:54:38 +0500

    feat: create comment entity

---
+++ src/comment/comment.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "../user/entities/user.entity";
+import { Blog } from "../blog/blog.entity";
+
+@Entity()
+export class Comment {
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @ManyToOne(() => User, (user) => user.id)
+    user!: User;
+
+    @ManyToOne(() => Blog, (blog) => blog.id)
+    blog!: Blog;
+
+    @Column('text')
+    context!: string;
+
+    @CreateDateColumn()
+    created_at!: Date;
+
+    @UpdateDateColumn()
+    updated_at!: Date;
+}
+    
